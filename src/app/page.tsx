4b0f65586e10a8b6c85a2a3889f8df134b53925f
Goksel Eryigit <goksel@carbonhealth.com>
2024-02-27 22:34:07 +0200

    kick off

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Calendar from "@/components/Calendar/Calendar";
 
 export default function Home() {
   return (
-    <main className="p-24 flex flex-col justify-center gap-8">
+    <main className="p-24 flex flex-col items-center gap-8">
 
       {Array.from({ length: 12 }).map((_, i) => (
         <Calendar key={i} now={new Date(2024, i, 14)} />
